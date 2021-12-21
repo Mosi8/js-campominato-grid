9674b3e19b391fb611fa10c6ca9e2f27cc1c896b
Mosi8 <mauriziomosi8@gmail.com>
2021-12-21 14:52:48 +0100

    done

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
-function cambioColor(cell){
-    for(i=0; i < cell.length; i++){
-        cell[i].addEventListener("click", function(){
+function cambioColor(cella){
+    for(i=0; i < cella.length; i++){
+        cella[i].addEventListener("click", function(){
             this.classList.add ("active");
         })
     }
@@ -15,34 +15,37 @@ let quadrato = document.getElementsByClassName('col-1');
 
 tasto.addEventListener('click',
 function () {
+    
     let livelloDiff = diff.options[diff.selectedIndex].value;
-
 
     console.log(livelloDiff);
 
     if (livelloDiff == 1){
+        campo.innerHTML = '';
         for (let i = 1; i <= 100; i++){
             console.log(i);
 
             campo.innerHTML += '<div class="col-1 block d-inline-block" id= "quadrato-' + [i] + '">' + (i) + '</div>';
             
-
-            let checked = cambioColor(quadrato);
+            cambioColor(quadrato);
         }
 
     }else if (livelloDiff == 2){
+        campo.innerHTML = '';
         for (let i = 1; i <= 81; i++){
         
             console.log(i);
             campo.innerHTML += '<div class="col-1 block block_9 d-inline-block">' + (i) + '</div>';
-            let checked = cambioColor(quadrato);
+            cambioColor(quadrato);
         }
-    }else 
+    }else {
+        campo.innerHTML = '';
         for (let i = 1; i <= 49; i++){
             
             console.log(i);
             campo.innerHTML += '<div class="col-1 block block_7 d-inline-block">' + (i) + '</div>';
-            let checked = cambioColor(quadrato);
+            cambioColor(quadrato);
+        }
     }
 
 
